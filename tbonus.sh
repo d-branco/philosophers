cfb51f6330025c3fde0b22deecc559cf6625a613
gris <185072671+d-branco@users.noreply.github.com>
2025-03-03 09:51:56 +0000

    testing sh

--- tbonus.sh
+++ tbonus.sh
@@ -1,44 +1,68 @@
 clear
 make bonus
+make clean
 
-philo_bonus/philo_bonus 4 310 200 100 42
-echo "		FIM de philo_bonus/philo_bonus 4 310 200 100 42                GOOD"
+philo_bonus/philo_bonus  1 800 200 200
+echo "		FIM de philo_bonus/philo_bonus  1 800 200 200
+		The philosopher should not eat and should die."
 sleep 2
-philo_bonus/philo_bonus 10 410 200 200 42
-echo "		FIM de philo_bonus/philo_bonus 10 410 200 200 42               GOOD"
+#philo_bonus/philo_bonus  5 400 100 100 42
+#echo "		FIM de philo_bonus/philo_bonus  5 400 100 100
+#		No philosopher should die."
+#sleep 2
+philo_bonus/philo_bonus  5 800 200 200 7
+echo "		FIM de philo_bonus/philo_bonus  5 800 200 200 7
+		No philosopher should die and the simulation should stop when every
+		philosopher has eaten at least 7 times."
 sleep 2
-philo_bonus/philo_bonus 5 800 200 200 42
-echo "		FIM de philo_bonus/philo_bonus 5 800 200 200 42                GOOD"
+philo_bonus/philo_bonus  4 410 200 200
+echo "		FIM de philo_bonus/philo_bonus  4 410 200 200
+		No philosopher should die."
 sleep 2
-philo_bonus/philo_bonus 4 410 200 200 42
-echo "		FIM de philo_bonus/philo_bonus 4 410 200 200 42                GOOD"
-sleep 2
-philo_bonus/philo_bonus 2  60  60  60 42
-echo "		FIM de philo_bonus/philo_bonus 2  60  60  60 42                GOOD"
-sleep 2
-philo_bonus/philo_bonus 10 410 200 200 42
-echo "		FIM de philo_bonus/philo_bonus 10 410 200 200 42               GOOD"
-sleep 2
-philo_bonus/philo_bonus 6 7 28 1 42
-echo "		FIM de philo_bonus/philo_bonus 6 7 28 1 42                     GOOD"
-sleep 2
-philo_bonus/philo_bonus 6 10 40 2 42
-echo "		FIM de philo_bonus/philo_bonus 6 10 40 2 42                    GOOD"
-sleep 1
-philo_bonus/philo_bonus 6 12 48 3 42
-echo "		FIM de philo_bonus/philo_bonus 6 12 48 3 42                    GOOD"
-sleep 1
-philo_bonus/philo_bonus 6 15 60 4 42
-echo "		FIM de philo_bonus/philo_bonus 6 15 60 4 42                    GOOD"
-sleep 1
-philo_bonus/philo_bonus 7 7 28 1 42
-echo "		FIM de philo_bonus/philo_bonus 7 7 28 1 42                     GOOD"
-sleep 1
-philo_bonus/philo_bonus 7 10 40 2 42
-echo "		FIM de philo_bonus/philo_bonus 7 10 40 2 42                    GOOD"
-sleep 1
-philo_bonus/philo_bonus 7 12 48 3 42
-echo "		FIM de philo_bonus/philo_bonus 7 12 48 3 42                    GOOD"
-sleep 1
-philo_bonus/philo_bonus 7 15 60 4 42
-echo "		FIM de philo_bonus/philo_bonus 7 15 60 4 42                    GOOD"
+philo_bonus/philo_bonus  4 310 200 100
+echo "		FIM de philo_bonus/philo_bonus  4 310 200 100
+		One philosopher should die."
+sleep 4
+
+
+#philo_bonus/philo_bonus 4 310 200 100 42
+#echo "		FIM de philo_bonus/philo_bonus 4 310 200 100 42                GOOD"
+#sleep 2
+#philo_bonus/philo_bonus 10 410 200 200 42
+#echo "		FIM de philo_bonus/philo_bonus 10 410 200 200 42               GOOD"
+#sleep 2
+#philo_bonus/philo_bonus 5 800 200 200 42
+#echo "		FIM de philo_bonus/philo_bonus 5 800 200 200 42                GOOD"
+#sleep 2
+#philo_bonus/philo_bonus 4 410 200 200 42
+#echo "		FIM de philo_bonus/philo_bonus 4 410 200 200 42                GOOD"
+#sleep 2
+#philo_bonus/philo_bonus 2  60  60  60 42
+#echo "		FIM de philo_bonus/philo_bonus 2  60  60  60 42                GOOD"
+#sleep 2
+#philo_bonus/philo_bonus 10 410 200 200 42
+#echo "		FIM de philo_bonus/philo_bonus 10 410 200 200 42               GOOD"
+#sleep 2
+#philo_bonus/philo_bonus 6 7 28 1 42
+#echo "		FIM de philo_bonus/philo_bonus 6 7 28 1 42                     GOOD"
+#sleep 2
+#philo_bonus/philo_bonus 6 10 40 2 42
+#echo "		FIM de philo_bonus/philo_bonus 6 10 40 2 42                    GOOD"
+#sleep 1
+#philo_bonus/philo_bonus 6 12 48 3 42
+#echo "		FIM de philo_bonus/philo_bonus 6 12 48 3 42                    GOOD"
+#sleep 1
+#philo_bonus/philo_bonus 6 15 60 4 42
+#echo "		FIM de philo_bonus/philo_bonus 6 15 60 4 42                    GOOD"
+#sleep 1
+#philo_bonus/philo_bonus 7 7 28 1 42
+#echo "		FIM de philo_bonus/philo_bonus 7 7 28 1 42                     GOOD"
+#sleep 1
+#philo_bonus/philo_bonus 7 10 40 2 42
+#echo "		FIM de philo_bonus/philo_bonus 7 10 40 2 42                    GOOD"
+#sleep 1
+#philo_bonus/philo_bonus 7 12 48 3 42
+#echo "		FIM de philo_bonus/philo_bonus 7 12 48 3 42                    GOOD"
+#sleep 1
+#philo_bonus/philo_bonus 7 15 60 4 42
+#echo "		FIM de philo_bonus/philo_bonus 7 15 60 4 42                    GOOD"
--- test.sh
+++ test.sh
@@ -1,44 +1,68 @@
 clear
 make philo
+make clean
 
-philo/philo 4 310 200 100 42
-echo "		FIM de philo/philo 4 310 200 100 42                            GOOD"
+philo/philo  1 800 200 200
+echo "		FIM de philo/philo  1 800 200 200
+		The philosopher should not eat and should die."
 sleep 2
-philo/philo 10 410 200 200 42
-echo "		FIM de philo/philo 10 410 200 200 42                           GOOD"
+#philo/philo  5 400 100 100 42
+#echo "		FIM de philo/philo  5 400 100 100
+#		No philosopher should die."
+#sleep 2
+philo/philo  5 800 200 200 7
+echo "		FIM de philo/philo  5 800 200 200 7
+		No philosopher should die and the simulation should stop when every
+		philosopher has eaten at least 7 times."
 sleep 2
-philo/philo 5 800 200 200 42
-echo "		FIM de philo/philo 5 800 200 200 42                            GOOD"
+philo/philo  4 410 200 200
+echo "		FIM de philo/philo  4 410 200 200
+		No philosopher should die."
 sleep 2
-philo/philo 4 410 200 200 42
-echo "		FIM de philo/philo 4 410 200 200 42                            GOOD"
-sleep 2
-philo/philo 2  60  60  60 42
-echo "		FIM de philo/philo 2  60  60  60 42                            GOOD"
-sleep 2
-philo/philo 10 410 200 200 42
-echo "		FIM de philo/philo 10 410 200 200 42                           GOOD"
-sleep 2
-philo/philo 6 7 28 1 42
-echo "		FIM de philo/philo 6 7 28 1 42                                 GOOD"
-sleep 2
-philo/philo 6 10 40 2 42
-echo "		FIM de philo/philo 6 10 40 2 42                                GOOD"
-sleep 2
-philo/philo 6 12 48 3 42
-echo "		FIM de philo/philo 6 12 48 3 42                                GOOD"
-sleep 2
-philo/philo 6 15 60 4 42
-echo "		FIM de philo/philo 6 15 60 4 42                                GOOD"
-sleep 2
-philo/philo 7 7 28 1 42
-echo "		FIM de philo/philo 7 7 28 1 42                                 GOOD"
-sleep 2
-philo/philo 7 10 40 2 42
-echo "		FIM de philo/philo 7 10 40 2 42                                GOOD"
-sleep 2
-philo/philo 7 12 48 3 42
-echo "		FIM de philo/philo 7 12 48 3 42                                GOOD"
-sleep 2
-philo/philo 7 15 60 4 42
-echo "		FIM de philo/philo 7 15 60 4 42                                GOOD"
+philo/philo  4 310 200 100
+echo "		FIM de philo/philo  4 310 200 100
+		One philosopher should die."
+sleep 4
+
+
+#philo/philo 4 310 200 100 42
+#echo "		FIM de philo/philo 4 310 200 100 42                            GOOD"
+#sleep 2
+#philo/philo 10 410 200 200 42
+#echo "		FIM de philo/philo 10 410 200 200 42                           GOOD"
+#sleep 2
+#philo/philo 5 800 200 200 42
+#echo "		FIM de philo/philo 5 800 200 200 42                            GOOD"
+#sleep 2
+#philo/philo 4 410 200 200 42
+#echo "		FIM de philo/philo 4 410 200 200 42                            GOOD"
+#sleep 2
+#philo/philo 2  60  60  60 42
+#echo "		FIM de philo/philo 2  60  60  60 42                            GOOD"
+#sleep 2
+#philo/philo 10 410 200 200 42
+#echo "		FIM de philo/philo 10 410 200 200 42                           GOOD"
+#sleep 2
+#philo/philo 6 7 28 1 42
+#echo "		FIM de philo/philo 6 7 28 1 42                                 GOOD"
+#sleep 2
+#philo/philo 6 10 40 2 42
+#echo "		FIM de philo/philo 6 10 40 2 42                                GOOD"
+#sleep 2
+#philo/philo 6 12 48 3 42
+#echo "		FIM de philo/philo 6 12 48 3 42                                GOOD"
+#sleep 2
+#philo/philo 6 15 60 4 42
+#echo "		FIM de philo/philo 6 15 60 4 42                                GOOD"
+#sleep 2
+#philo/philo 7 7 28 1 42
+#echo "		FIM de philo/philo 7 7 28 1 42                                 GOOD"
+#sleep 2
+#philo/philo 7 10 40 2 42
+#echo "		FIM de philo/philo 7 10 40 2 42                                GOOD"
+#sleep 2
+#philo/philo 7 12 48 3 42
+#echo "		FIM de philo/philo 7 12 48 3 42                                GOOD"
+#sleep 2
+#philo/philo 7 15 60 4 42
+#echo "		FIM de philo/philo 7 15 60 4 42                                GOOD"
